a82eac0688604415bda95f973d8472962aab8b85
Nitay161 <157272945+Nitay161@users.noreply.github.com>
2025-12-06 20:54:59 +0200

    Add files via upload

---
+++ drive-project-main/src/input/DeleteInput.cpp
@@ -0,0 +1,23 @@
+#include "DeleteInput.h"
+#include <stdexcept>
+
+vector<string> DeleteInput::interpret(string input) {
+    vector<string> args;
+    // Find the first space.
+    int pos = input.find(' ');
+    // If no spaces then the format is wrong.
+    if (pos == string::npos) {
+        throw invalid_argument("400 Bad Request");
+    }
+    args.push_back(input.substr(0, pos));
+    input = input.substr(pos + 1);
+
+    // Find the second space.
+    pos = input.find(' ');
+    // If there are spaces then the format is wrong.
+    if (pos != string::npos) {
+        throw invalid_argument("400 Bad Request");
+    }
+    args.push_back(input);
+    return args;
+}
---
+++ drive-project-main/src/input/InputManager.cpp
@@ -0,0 +1,26 @@
+#include "InputManager.h"
+#include <stdexcept>
+
+InputManager::InputManager(map<string, ICInput*> inputs) : inputs(inputs) {}
+
+vector<string> InputManager::interpret(string input) {
+    // Find the first space.
+    int pos = input.find(' ');
+    // If no spaces then the format is wrong.
+    if (pos == string::npos) {
+        throw invalid_argument("400 Bad Request");
+    }
+    string cmd = input.substr(0, pos);
+
+    // Checking if the command name exists.
+    bool isExist = false;
+    for (const auto& pair : inputs) {
+        if (pair.first == cmd) {
+            isExist = true;
+            break;
+        }
+    }
+    if (!isExist) throw invalid_argument("400 Bad Request");
+
+    return inputs[cmd]->interpret(input);
+}
