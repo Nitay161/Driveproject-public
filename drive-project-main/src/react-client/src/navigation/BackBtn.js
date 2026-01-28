cf281a84f05acc34952dceaa2d25a4c3bb5f4e12
Nitay161 <157272945+Nitay161@users.noreply.github.com>
2026-01-28 14:32:41 +0200

    Delete drive-project-main directory

--- drive-project-main/src/react-client/src/navigation/BackBtn.js
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-
-function BackBtn() {
-    return (
-        <Link to="/" className="btn btn-secondary me-2">Back</Link>
-    );
-}
-
-export default BackBtn;
--- drive-project-main/src/web-server/routes/search.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require('../controllers/files')
-
-router.route('/:query')
-    .get(controller.searchByQuery)
-
-module.exports = router
